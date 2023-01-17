7ef920f6af76af1ca6f1b5ee4b0c2abc6474e1ae
leaf_go <leaf_go@126.com>
2023-01-17 11:58:35 +0800

    x

--- hash.go
+++ hash.go
@@ -1,21 +1,57 @@
 package hash
 
-import "crypto"
+import (
+	"crypto"
+	_ "crypto/md5"
+	_ "crypto/sha1"
+	_ "crypto/sha256"
+)
 
-func Md5(data []byte) []byte {
+type Type struct {
+	data interface{}
+}
+
+func NewType(data interface{}) *Type {
+	return &Type{data: data}
+}
+
+func (t Type) Bytes() []byte {
+	switch t.data.(type) {
+	case []byte:
+		return t.data.([]byte)
+	case string:
+		return []byte(t.data.(string))
+	}
+
+	panic("not allowed type")
+}
+
+func (t Type) String() string {
+	switch t.data.(type) {
+	case []byte:
+		return string(t.data.([]byte))
+	case string:
+		return t.data.(string)
+	}
+
+	panic("not allowed type")
+}
+
+func Md5(data interface{}) *Type {
 	return doHash(crypto.MD5, data)
 }
 
-func Sha1(data []byte) []byte {
+func Sha1(data interface{}) *Type {
 	return doHash(crypto.SHA1, data)
 }
 
-func Sha256(data []byte) []byte {
+func Sha256(data interface{}) *Type {
 	return doHash(crypto.SHA256, data)
 }
 
-func doHash(hash crypto.Hash, data []byte) []byte {
+func doHash(hash crypto.Hash, data interface{}) *Type {
+	t := NewType(data)
 	object := hash.New()
-	object.Write(data)
-	return object.Sum(nil)
+	object.Write(t.Bytes())
+	return NewType(object.Sum(nil))
 }
